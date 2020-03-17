84b2da000f767ece1bf3588a989dcc7f325be9d0
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-17 17:06:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,13 +58,13 @@ end
     grade = row[2]
     self.new(id, name, grade)
   end 
-def self.new_from_db(row)
-new_dog = self.new  # self.new is the same as running Song.new
-new_dog.id = row[0]
-new_dog.name =  row[1]
-new_dog.breed = row[2]
-#new_dog  # return the newly created instance
-end
+# def self.new_from_db(row)
+# new_dog = self.new  # self.new is the same as running Song.new
+# new_dog.id = row[0]
+# new_dog.name =  row[1]
+# new_dog.breed = row[2]
+# new_dog  # return the newly created instance
+# end
 
 
 
